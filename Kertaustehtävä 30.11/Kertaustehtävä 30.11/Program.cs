c203e48ed192db7cbec0375ce9552845cea3909c
renupetori <31991591+renupetori@users.noreply.github.com>
2017-12-14 13:53:00 +0200

    Valmis

---
+++ Kertaustehtävä 30.11/Kertaustehtävä 30.11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kertaustehtävä_30._11
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] prepositiot = { "of", "the" };
+
+            Console.WriteLine("Kirjoita lause missä on isoja ja pieniä kirjaimia:");
+            Console.WriteLine();
+            string lause = Console.ReadLine();
+            Toiminnot p = new Toiminnot (lause);
+            Console.WriteLine(p.Tulostus());
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ Kertaustehtävä 30.11/Kertaustehtävä 30.11/Toiminnot.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Kertaustehtävä_30._11
+{
+    class Toiminnot
+    {
+
+        private string _input;
+
+
+        public Toiminnot(string input)
+        {
+
+            _input = input;
+            
+            _input = new System.Globalization.CultureInfo("en-US", false).TextInfo.ToTitleCase(_input.ToLower());
+            
+            if (_input.Contains(" The ") == true){
+                _input = _input.Replace(" The ", " the ");
+            }
+
+            if (_input.Contains(" Of ") == true)
+            {
+                _input = _input.Replace(" Of ", " of ");
+            }
+
+        }
+
+        
+
+        public string Tulostus()
+        {
+
+            return _input;
+
+        }
+
+    }
+}
