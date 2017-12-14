d602ab9d5aa38f5a53f308fb8564d420763a0df8
renupetori <31991591+renupetori@users.noreply.github.com>
2017-12-14 20:04:30 +0200

    Kesken

---
+++ Harjoituspeli/Harjoituspeli/LevelUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+
+
+namespace Harjoituspeli
+{
+    class LevelUp
+    {
+        int points;
+
+        public LevelUp()
+        {
+            Console.WriteLine("Level up!");
+            
+            
+            Random rnd = new Random();
+            points = rnd.Next(3);
+
+            Console.WriteLine($"STR: {Character._stats["STR"]} +{points}");
+            Character._stats["STR"] = Character._stats["STR"] + points;
+            Character._stats["DEX"] = Character._stats["DEX"] + points;
+            Character._stats["VIT"] = Character._stats["VIT"] + points;
+            Character._stats["INT"] = Character._stats["INT"] + points;
+            Character._stats["MND"] = Character._stats["MND"] + points;
+            Character._stats["LCK"] = Character._stats["LCK"] + points;
+            return;
+        }
+        
+
+        
+    }
+}
